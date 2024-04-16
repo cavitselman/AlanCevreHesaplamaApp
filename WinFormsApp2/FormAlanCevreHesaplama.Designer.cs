b78f695a1aa843f32c4fd3e2b8af8d914b8a0be1
Cavit Selman <tetikci462@hotmail.com>
2024-04-16 13:34:27 +0300

    Final

---
+++ WinFormsApp2/FormAlanCevreHesaplama.Designer.cs
@@ -0,0 +1,438 @@
+﻿namespace WinFormsApp2
+{
+    partial class FormAlanCevreHesaplama
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAlanCevreHesaplama));
+            panel1 = new Panel();
+            label1 = new Label();
+            panel2 = new Panel();
+            label11 = new Label();
+            label10 = new Label();
+            textBoxKenar = new TextBox();
+            LblKareCevre = new Label();
+            LblKareAlan = new Label();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            panel3 = new Panel();
+            label17 = new Label();
+            label16 = new Label();
+            textBoxYaricap = new TextBox();
+            LblDaireCevre = new Label();
+            LblDaireAlan = new Label();
+            label8 = new Label();
+            label6 = new Label();
+            pictureBox3 = new PictureBox();
+            panel4 = new Panel();
+            label13 = new Label();
+            label12 = new Label();
+            textBoxUzunKenar = new TextBox();
+            textBoxKisaKenar = new TextBox();
+            LblDikdortgenCevre = new Label();
+            LblDikdortgenAlan = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            pictureBox2 = new PictureBox();
+            BtnAlanHesapla = new Button();
+            BtnCevreHesapla = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(192, 255, 255);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(0, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1199, 147);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Comic Sans MS", 27.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(280, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(632, 52);
+            label1.TabIndex = 0;
+            label1.Text = "Alan Çevre Hesaplama Uygulaması";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(255, 192, 192);
+            panel2.Controls.Add(label11);
+            panel2.Controls.Add(label10);
+            panel2.Controls.Add(textBoxKenar);
+            panel2.Controls.Add(LblKareCevre);
+            panel2.Controls.Add(LblKareAlan);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Location = new Point(0, 154);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(401, 338);
+            panel2.TabIndex = 1;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(132, 299);
+            label11.Name = "label11";
+            label11.Size = new Size(40, 15);
+            label11.TabIndex = 3;
+            label11.Text = "Çevre:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(138, 276);
+            label10.Name = "label10";
+            label10.Size = new Size(34, 15);
+            label10.TabIndex = 3;
+            label10.Text = "Alan:";
+            // 
+            // textBoxKenar
+            // 
+            textBoxKenar.Location = new Point(175, 238);
+            textBoxKenar.Name = "textBoxKenar";
+            textBoxKenar.Size = new Size(100, 23);
+            textBoxKenar.TabIndex = 2;
+            textBoxKenar.Text = "0";
+            // 
+            // LblKareCevre
+            // 
+            LblKareCevre.AutoSize = true;
+            LblKareCevre.Location = new Point(175, 300);
+            LblKareCevre.Name = "LblKareCevre";
+            LblKareCevre.Size = new Size(13, 15);
+            LblKareCevre.TabIndex = 1;
+            LblKareCevre.Text = "0";
+            // 
+            // LblKareAlan
+            // 
+            LblKareAlan.AutoSize = true;
+            LblKareAlan.Location = new Point(175, 278);
+            LblKareAlan.Name = "LblKareAlan";
+            LblKareAlan.Size = new Size(13, 15);
+            LblKareAlan.TabIndex = 1;
+            LblKareAlan.Text = "0";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(110, 242);
+            label2.Name = "label2";
+            label2.Size = new Size(62, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Kenar (m):";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(61, 18);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(277, 199);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(255, 255, 192);
+            panel3.Controls.Add(label17);
+            panel3.Controls.Add(label16);
+            panel3.Controls.Add(textBoxYaricap);
+            panel3.Controls.Add(LblDaireCevre);
+            panel3.Controls.Add(LblDaireAlan);
+            panel3.Controls.Add(label8);
+            panel3.Controls.Add(label6);
+            panel3.Controls.Add(pictureBox3);
+            panel3.Location = new Point(806, 154);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(393, 338);
+            panel3.TabIndex = 1;
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Location = new Point(130, 309);
+            label17.Name = "label17";
+            label17.Size = new Size(40, 15);
+            label17.TabIndex = 3;
+            label17.Text = "Çevre:";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Location = new Point(136, 286);
+            label16.Name = "label16";
+            label16.Size = new Size(34, 15);
+            label16.TabIndex = 3;
+            label16.Text = "Alan:";
+            // 
+            // textBoxYaricap
+            // 
+            textBoxYaricap.Location = new Point(173, 246);
+            textBoxYaricap.Name = "textBoxYaricap";
+            textBoxYaricap.Size = new Size(100, 23);
+            textBoxYaricap.TabIndex = 2;
+            textBoxYaricap.Text = "0";
+            // 
+            // LblDaireCevre
+            // 
+            LblDaireCevre.AutoSize = true;
+            LblDaireCevre.Location = new Point(173, 310);
+            LblDaireCevre.Name = "LblDaireCevre";
+            LblDaireCevre.Size = new Size(13, 15);
+            LblDaireCevre.TabIndex = 1;
+            LblDaireCevre.Text = "0";
+            // 
+            // LblDaireAlan
+            // 
+            LblDaireAlan.AutoSize = true;
+            LblDaireAlan.Location = new Point(173, 286);
+            LblDaireAlan.Name = "LblDaireAlan";
+            LblDaireAlan.Size = new Size(13, 15);
+            LblDaireAlan.TabIndex = 1;
+            LblDaireAlan.Text = "0";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(122, 249);
+            label8.Name = "label8";
+            label8.Size = new Size(48, 15);
+            label8.TabIndex = 1;
+            label8.Text = "Yarıçap:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(-230, 309);
+            label6.Name = "label6";
+            label6.Size = new Size(38, 15);
+            label6.TabIndex = 1;
+            label6.Text = "label2";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(22, 18);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(361, 199);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.Silver;
+            panel4.Controls.Add(label13);
+            panel4.Controls.Add(label12);
+            panel4.Controls.Add(textBoxUzunKenar);
+            panel4.Controls.Add(textBoxKisaKenar);
+            panel4.Controls.Add(LblDikdortgenCevre);
+            panel4.Controls.Add(LblDikdortgenAlan);
+            panel4.Controls.Add(label4);
+            panel4.Controls.Add(label3);
+            panel4.Controls.Add(pictureBox2);
+            panel4.Location = new Point(407, 154);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(393, 338);
+            panel4.TabIndex = 1;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(123, 317);
+            label13.Name = "label13";
+            label13.Size = new Size(40, 15);
+            label13.TabIndex = 3;
+            label13.Text = "Çevre:";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(129, 294);
+            label12.Name = "label12";
+            label12.Size = new Size(34, 15);
+            label12.TabIndex = 3;
+            label12.Text = "Alan:";
+            // 
+            // textBoxUzunKenar
+            // 
+            textBoxUzunKenar.Location = new Point(167, 262);
+            textBoxUzunKenar.Name = "textBoxUzunKenar";
+            textBoxUzunKenar.Size = new Size(100, 23);
+            textBoxUzunKenar.TabIndex = 2;
+            textBoxUzunKenar.Text = "0";
+            // 
+            // textBoxKisaKenar
+            // 
+            textBoxKisaKenar.Location = new Point(167, 233);
+            textBoxKisaKenar.Name = "textBoxKisaKenar";
+            textBoxKisaKenar.Size = new Size(100, 23);
+            textBoxKisaKenar.TabIndex = 2;
+            textBoxKisaKenar.Text = "0";
+            // 
+            // LblDikdortgenCevre
+            // 
+            LblDikdortgenCevre.AutoSize = true;
+            LblDikdortgenCevre.Location = new Point(166, 318);
+            LblDikdortgenCevre.Name = "LblDikdortgenCevre";
+            LblDikdortgenCevre.Size = new Size(13, 15);
+            LblDikdortgenCevre.TabIndex = 1;
+            LblDikdortgenCevre.Text = "0";
+            // 
+            // LblDikdortgenAlan
+            // 
+            LblDikdortgenAlan.AutoSize = true;
+            LblDikdortgenAlan.Location = new Point(166, 294);
+            LblDikdortgenAlan.Name = "LblDikdortgenAlan";
+            LblDikdortgenAlan.Size = new Size(13, 15);
+            LblDikdortgenAlan.TabIndex = 1;
+            LblDikdortgenAlan.Text = "0";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(94, 265);
+            label4.Name = "label4";
+            label4.Size = new Size(70, 15);
+            label4.TabIndex = 1;
+            label4.Text = "Uzun Kenar:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(100, 236);
+            label3.Name = "label3";
+            label3.Size = new Size(64, 15);
+            label3.TabIndex = 1;
+            label3.Text = "Kısa Kenar:";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(29, 18);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(325, 199);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // BtnAlanHesapla
+            // 
+            BtnAlanHesapla.BackColor = Color.FromArgb(128, 64, 64);
+            BtnAlanHesapla.Location = new Point(266, 524);
+            BtnAlanHesapla.Name = "BtnAlanHesapla";
+            BtnAlanHesapla.Size = new Size(707, 57);
+            BtnAlanHesapla.TabIndex = 2;
+            BtnAlanHesapla.Text = "ALAN HESAPLA";
+            BtnAlanHesapla.UseVisualStyleBackColor = false;
+            BtnAlanHesapla.Click += BtnAlanHesapla_Click;
+            // 
+            // BtnCevreHesapla
+            // 
+            BtnCevreHesapla.BackColor = Color.FromArgb(0, 192, 0);
+            BtnCevreHesapla.Location = new Point(266, 600);
+            BtnCevreHesapla.Name = "BtnCevreHesapla";
+            BtnCevreHesapla.Size = new Size(707, 57);
+            BtnCevreHesapla.TabIndex = 2;
+            BtnCevreHesapla.Text = "ÇEVRE HESAPLA";
+            BtnCevreHesapla.UseVisualStyleBackColor = false;
+            BtnCevreHesapla.Click += BtnCevreHesapla_Click;
+            // 
+            // FormAlanCevreHesaplama
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1201, 689);
+            Controls.Add(BtnCevreHesapla);
+            Controls.Add(BtnAlanHesapla);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "FormAlanCevreHesaplama";
+            Text = "Alan Çevre Hesaplama";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private TextBox textBoxKenar;
+        private Label LblKareAlan;
+        private Label label2;
+        private TextBox textBoxYaricap;
+        private Label LblDaireAlan;
+        private Label label8;
+        private Label label6;
+        private TextBox textBoxUzunKenar;
+        private TextBox textBoxKisaKenar;
+        private Label LblDikdortgenAlan;
+        private Label label4;
+        private Label label3;
+        private Button BtnAlanHesapla;
+        private Button BtnCevreHesapla;
+        private Label label11;
+        private Label label10;
+        private Label LblKareCevre;
+        private Label label17;
+        private Label label16;
+        private Label LblDaireCevre;
+        private Label label13;
+        private Label label12;
+        private Label LblDikdortgenCevre;
+    }
+}
---
+++ WinFormsApp2/FormAlanCevreHesaplama.cs
@@ -0,0 +1,48 @@
+﻿namespace WinFormsApp2
+{
+    public partial class FormAlanCevreHesaplama : Form
+    {
+        public FormAlanCevreHesaplama()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAlanHesapla_Click(object sender, EventArgs e)
+        {
+            //Kare Alan Hesaplama
+            int kareKenarUzunluk = Convert.ToInt32(textBoxKenar.Text);
+            int alanKare = kareKenarUzunluk * kareKenarUzunluk;
+            LblKareAlan.Text = alanKare.ToString();
+
+            //Dikdörtgen Alan Hesaplama
+            int kisaKenarUzunluk = Convert.ToInt16(textBoxKisaKenar.Text);
+            int uzunKenarUzunluk = Convert.ToInt16(textBoxUzunKenar.Text);
+            int alanDikdörtgen = kisaKenarUzunluk * uzunKenarUzunluk;
+            LblDikdortgenAlan.Text = alanDikdörtgen.ToString();
+
+            //Daire Alan Hesaplama
+            int yaricap = Convert.ToInt32(textBoxYaricap.Text);
+            int alanDaire = (int)Math.PI * (yaricap * yaricap);
+            LblDaireAlan.Text = alanDaire.ToString();
+        }
+
+        private void BtnCevreHesapla_Click(object sender, EventArgs e)
+        {
+            //Kare Çevre Hesaplama
+            int kareKenarUzunluk = Convert.ToInt32(textBoxKenar.Text);
+            int cevreKare = kareKenarUzunluk * 4;
+            LblKareCevre.Text = cevreKare.ToString();
+
+            //Dikdörtgen Çevre Hesaplama
+            int kisaKenarUzunluk = Convert.ToInt16(textBoxKisaKenar.Text);
+            int uzunKenarUzunluk = Convert.ToInt16(textBoxUzunKenar.Text);
+            int cevreDikdörtgen = 2 * (kisaKenarUzunluk + uzunKenarUzunluk);
+            LblDikdortgenCevre.Text = cevreDikdörtgen.ToString();
+
+            //Daire Çevre Hesaplama
+            int yaricap = Convert.ToInt32(textBoxYaricap.Text);
+            int cevreDaire = 2 * (int)Math.PI * yaricap;
+            LblDaireCevre.Text = cevreDaire.ToString();
+        }
+    }
+}
